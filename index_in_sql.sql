17b403ba1ce902e24370697518c96655bdf17ada
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-11-21 15:38:10 +0700

    up files

---
+++ index_in_sql.sql
@@ -0,0 +1,14 @@
+use classicmodels;
+
+-- explain select * from customers where customerName = "Land Of Toys Inc.";
+
+-- ALTER TABLE customers ADD INDEX idx_customerName(customerName);
+
+-- EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
+
+-- EXPLAIN SELECT * FROM customers WHERE customerName = 'Double Decker Gift Stores, Ltd';
+
+ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
+EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
+
+-- ALTER TABLE customers DROP INDEX idx_full_name;
